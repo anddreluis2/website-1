4d9f145df51c794165c9febc32e16b5b58fc2274
anddreluis2 <anddreluis98@gmail.com>
2022-12-20 22:23:45 -0300

    changed name width

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -3,7 +3,7 @@ import { FaReact } from "react-icons/fa";
 export const InfoCard: React.FC = ({}) => {
   return (
     <>
-      <div className="bg-background w-1/3 justify-center m-auto mt-[200px]">
+      <div className="bg-background w-full justify-center m-auto mt-[200px]">
         <h1 className="text-white p-6 pb-4 justify-center flex text-exl">
           Andre Luis de Oliveira Andrade
         </h1>
