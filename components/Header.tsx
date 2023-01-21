f355bc7e1772ddbd281e977489885a23c59f0c56
anddreluis2 <anddreluis98@gmail.com>
2023-01-21 17:34:00 -0300

    feat: added name to webpage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import React, { useState } from "react";
 import { DiGit } from "react-icons/di";
 
-export const Header: React.FC = ({}) => {
+export const Header: React.FC = () => {
+  const [whiteMode, setWhiteMode] = useState(false);
+
   return (
-    <div className="bg-header w-[100vw] p-6 h-20">
-      <Link className="text-white items-center" href="/">
-        <DiGit size={40} />
-      </Link>
+    <div className="bg-header w-[100vw] justify-between flex p-6 h-20">
+      <div className="w-12">
+        <Link className="text-white items-center" href="/">
+          <DiGit size={40} />
+        </Link>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
+import { useState } from "react";
 import { Header, InfoCard, SocialCard } from "../components";
 
 export default function Home() {
   return (
     <div className="w-[100vw] h-[100vh] bg-background">
+      <Head>
+        <title>Andre Luis de Oliveira</title>
+      </Head>
       <Header />
       <InfoCard />
       <SocialCard />
