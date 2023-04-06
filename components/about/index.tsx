ef017219dd1b60c861b72f95886a0c00d35116fc
anddreluis2 <anddreluis98@gmail.com>
2023-04-05 23:37:25 -0300

    added right cv

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -25,8 +25,8 @@ export default function About() {
                         <div className="w-full pt-8 flex justify-center">
                             {' '}
                             <a
-                                download="https://cdn.discordapp.com/attachments/880206315203481603/1093359871816114236/Andre_Luis_de_Oliveira_Andrade.pdf"
-                                href="https://cdn.discordapp.com/attachments/880206315203481603/1093359871816114236/Andre_Luis_de_Oliveira_Andrade.pdf"
+                                download="https://cdn.discordapp.com/attachments/880206315203481603/1093363620995407932/Curriculo_AndreLuis.pdf"
+                                href="https://cdn.discordapp.com/attachments/880206315203481603/1093363620995407932/Curriculo_AndreLuis.pdf"
                                 className="flex gap-3 text-white justify-center hover:text-gray items-center"
                             >
                                 <FaDownload size={20} /> Download my resume
