3049d7c7e9e54df0ebdb757965fc5c73381178ec
Andre Luis <anddreluis98@gmail.com>
2023-07-24 22:02:54 -0300

    testing another way

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
-import type { AppProps } from "next/app";
-import "styles/global.css";
-import { Source_Code_Pro } from "next/font/google";
+import type { AppProps } from 'next/app'
+import 'styles/global.css'
+import { Source_Code_Pro } from 'next/font/google'
+import Script from 'next/script'
 
 const gruppo = Source_Code_Pro({
-  style: ["normal"],
-  subsets: ["latin"],
-  weight: "400",
-});
+    style: ['normal'],
+    subsets: ['latin'],
+    weight: '400',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={gruppo.className}>
-      <Component {...pageProps} />
-    </main>
-  );
+    return (
+        <main className={gruppo.className}>
+            <head>
+                <Script src="https://tools.luckyorange.com/core/lo.js?site-id=1035f454" />
+            </head>
+            <Component {...pageProps} />
+        </main>
+    )
 }
